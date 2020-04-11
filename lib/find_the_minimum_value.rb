fb8b58265654ada82ec146232ba73abb2b887d1a
Ewelina Szoda <ewelinaszoda@gmail.com>
2020-04-11 13:19:13 +0000

    Automatically backed up by Learn

--- lib/find_the_minimum_value.rb
+++ lib/find_the_minimum_value.rb
@@ -1,3 +1,9 @@
 def find_min_value(array)
- array.min 
+ count = 0 
+ value_min = array[0]
+ while array[count] < array.length do 
+   if value_min > array[count]
+     value_min = array[count]
+   end 
+   array[count]
 end
